e97c80892d4bf07cb7febf78feaef547e2ab3ed3
ucupofcoffe <92680687+nadyafrisca@users.noreply.github.com>
2022-10-31 09:31:06 +0700

    Create updatemenu.vue

---
+++ VUE/src/views/updatemenu.vue
@@ -0,0 +1,198 @@
+<template>
+  <div class="updatemenu-container">
+    <div class="updatemenu-updatemenu">
+      <img
+        alt="Ellipse313"
+        src="/playground_assets/ellipse313-maic9-200h.png"
+        class="updatemenu-ellipse3"
+      />
+      <div class="updatemenu-navbar">
+        <img
+          alt="icon15"
+          src="/playground_assets/icon15-5iwg.svg"
+          class="updatemenu-icon"
+        />
+        <div class="updatemenu-pages">
+          <img
+            alt="image318"
+            src="/playground_assets/image318-3a9c-200h.png"
+            class="updatemenu-image3"
+          />
+          <img
+            alt="Rectangle1919"
+            src="/playground_assets/rectangle1919-0xr-200h.png"
+            class="updatemenu-rectangle19"
+          />
+          <img
+            alt="Rectangle20110"
+            src="/playground_assets/rectangle20110-9xog-200h.png"
+            class="updatemenu-rectangle20"
+          />
+        </div>
+        <span class="updatemenu-text"><span>Mitra 211511021</span></span>
+        <span class="updatemenu-text02"><span>Meisya Puteri</span></span>
+        <span class="updatemenu-text04"><span>Menu</span></span>
+        <span class="updatemenu-text06"><span>Menu</span></span>
+      </div>
+      <div class="updatemenu-footer">
+        <span class="updatemenu-text08">
+          <span>Copyrights by GeoWisata.id</span>
+        </span>
+      </div>
+      <div class="updatemenu-side-bar">
+        <div class="updatemenu-log-out">
+          <img
+            alt="Rectangle13121"
+            src="/playground_assets/rectangle13121-qabq-200h.png"
+            class="updatemenu-rectangle13"
+          />
+          <span class="updatemenu-text10"><span>Add Mitra</span></span>
+          <img
+            alt="plussolid1123"
+            src="/playground_assets/plussolid1123-66de.svg"
+            class="updatemenu-plussolid1"
+          />
+        </div>
+        <div class="updatemenu-side-bar1">
+          <div class="updatemenu-resto">
+            <span class="updatemenu-text12"><span>Restaurant</span></span>
+            <div class="updatemenu-icon1">
+              <img
+                alt="Logo131"
+                src="/playground_assets/logo131-vzfq.svg"
+                class="updatemenu-logo"
+              />
+            </div>
+          </div>
+        </div>
+        <div class="updatemenu-log-out1">
+          <img
+            alt="Rectangle13135"
+            src="/playground_assets/rectangle13135-z5q5-200h.png"
+            class="updatemenu-rectangle131"
+          />
+          <img
+            alt="housesolid1136"
+            src="/playground_assets/housesolid1136-d49.svg"
+            class="updatemenu-housesolid1"
+          />
+          <span class="updatemenu-text14"><span>LogOut</span></span>
+        </div>
+        <div class="updatemenu-kategori">
+          <div class="updatemenu-drop-down">
+            <div class="updatemenu-restaurant">
+              <div class="updatemenu-icon2"></div>
+              <span class="updatemenu-text16"><span>Customer</span></span>
+            </div>
+            <div class="updatemenu-penginapan">
+              <div class="updatemenu-icon3"></div>
+              <span class="updatemenu-text18"><span>Menu</span></span>
+            </div>
+            <div class="updatemenu-tour-travel">
+              <div class="updatemenu-icon4"></div>
+              <span class="updatemenu-text20"><span>Chart</span></span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="updatemenu-aksi">
+        <div class="updatemenu-group7">
+          <img
+            alt="suspend156"
+            src="/playground_assets/suspend156-04y7.svg"
+            class="updatemenu-suspend"
+          />
+          <span class="updatemenu-text22"><span>Edit</span></span>
+        </div>
+        <div class="updatemenu-status">
+          <span class="updatemenu-text24"><span>Delete</span></span>
+        </div>
+        <div class="updatemenu-status1">
+          <span class="updatemenu-text26"><span>Back</span></span>
+        </div>
+      </div>
+      <div class="updatemenu-user">
+        <span class="updatemenu-text28"><span>Nama Produk</span></span>
+        <span class="updatemenu-text30"><span>Masukan Photo Produk</span></span>
+        <img
+          alt="Rectangle303167"
+          src="/playground_assets/rectangle303167-r6bdv-200h.png"
+          class="updatemenu-rectangle303"
+        />
+      </div>
+      <div class="updatemenu-user1">
+        <span class="updatemenu-text32"><span>Deskripsi Produk</span></span>
+        <img
+          alt="Rectangle303170"
+          src="/playground_assets/rectangle303170-tvbr-300h.png"
+          class="updatemenu-rectangle3031"
+        />
+      </div>
+      <div class="updatemenu-user2">
+        <span class="updatemenu-text34"><span>Harga</span></span>
+        <img
+          alt="Rectangle303173"
+          src="/playground_assets/rectangle303173-47i-200h.png"
+          class="updatemenu-rectangle3032"
+        />
+        <img
+          alt="Rectangle306174"
+          src="/playground_assets/rectangle306174-l0vk-200h.png"
+          class="updatemenu-rectangle306"
+        />
+        <img
+          alt="Rectangle304175"
+          src="/playground_assets/rectangle304175-lsqc.svg"
+          class="updatemenu-rectangle304"
+        />
+        <span class="updatemenu-text36"><span>Rp.</span></span>
+      </div>
+      <div class="updatemenu-user3">
+        <span class="updatemenu-text38"><span>Jenis</span></span>
+        <img
+          alt="Rectangle303179"
+          src="/playground_assets/rectangle303179-0fkj-200h.png"
+          class="updatemenu-rectangle3033"
+        />
+        <img
+          alt="Rectangle306180"
+          src="/playground_assets/rectangle306180-l44-200h.png"
+          class="updatemenu-rectangle3061"
+        />
+        <span class="updatemenu-text40"><span>Makanan</span></span>
+        <img
+          alt="Rectangle304182"
+          src="/playground_assets/rectangle304182-3dwm.svg"
+          class="updatemenu-rectangle3041"
+        />
+      </div>
+      <div class="updatemenu-btn">
+        <img
+          alt="Rectangle305184"
+          src="/playground_assets/rectangle305184-6gar.svg"
+          class="updatemenu-rectangle305"
+        />
+        <span class="updatemenu-text42"><span>Upload Photo</span></span>
+      </div>
+      <img
+        alt="Vector2186"
+        src="/playground_assets/vector2186-h56q.svg"
+        class="updatemenu-vector2"
+      />
+      <img
+        alt="image8187"
+        src="/playground_assets/image8187-ndwx-400h.png"
+        class="updatemenu-image8"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Updatemenu',
+  metaInfo: {
+    title: 'exported project',
+  },
+}
+</script>
